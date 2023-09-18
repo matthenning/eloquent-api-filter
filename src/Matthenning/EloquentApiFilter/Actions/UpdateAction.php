4c77f7f58ca92f8bb06b8cff796f8e9e1f45ad50
matthenning <mail@matthias-henning.de>
2023-09-18 20:33:55 +0200

    removed auto updating model relations for now

--- src/Matthenning/EloquentApiFilter/Actions/UpdateAction.php
+++ src/Matthenning/EloquentApiFilter/Actions/UpdateAction.php
@@ -35,12 +35,10 @@ class UpdateAction extends ActionAbstract
     /**
      * @return Model
      * @throws MissingRequestFieldException
-     * @throws ReflectionException
-     * @throws UnhandleableRelationshipException
      */
     public function invoke(): Model
     {
-        $this->updateModel()->updateRelations();
+        $this->updateModel();
 
         $this->model->save();
 
@@ -50,8 +48,6 @@ class UpdateAction extends ActionAbstract
     /**
      * @return Model
      * @throws MissingRequestFieldException
-     * @throws ReflectionException
-     * @throws UnhandleableRelationshipException
      */
     public function __invoke()
     {
@@ -80,92 +76,6 @@ class UpdateAction extends ActionAbstract
                 if ($this->relations->has($field)) {
                     $this->model->$fillable = $this->relations->get($field);
                 }
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return $this
-     * @throws ReflectionException
-     * @throws UnhandleableRelationshipException
-     */
-    protected function updateRelations(): self
-    {
-        // Remove related models which were not present in the update query at all
-        foreach ($this->model::getRelationNames() as $name => $relation) {
-            if (!$this->relations->contains($name)) {
-                switch ($relation) {
-                    case 'BelongsTo':
-                    case 'MorphTo':
-                        $this->model->$name()->dissociate();
-                        break;
-
-                    case 'BelongsToMany':
-                    case 'MorphedByMany':
-                    case 'MorphToMany':
-                        $this->model->$name()->detach();
-                        break;
-                }
-            }
-        }
-
-        if (!$this->relations) return $this;
-
-        // Sync relations from query
-        foreach ($this->relations as $name => $ids) {
-            $relation = $this->model::getRelationNames()[$name];
-            $model_name = $this->model->$name()->getRelated();
-            $models = $model_name::findMany($ids);
-
-            switch ($relation) {
-                case 'BelongsTo':
-                case 'MorphTo':
-                    $this->model->$name()->associate($ids);
-                    break;
-
-                case 'BelongsToMany':
-                case 'MorphedByMany':
-                case 'MorphToMany':
-                    $this->model->$name()->sync($ids);
-                    break;
-
-                case 'HasOne':
-                case 'MorphOne':
-                    $this->model->$name()->save($ids);
-                    break;
-
-                case 'HasMany':
-                    // Emulating a sync method by first removing and then adding related models
-                    $fk = $this->model->$name()->getForeignKeyName();
-                    $this->model->$name->filter(function (Model $m) use ($ids) {
-                        return !in_array($m->id, $ids);
-                    })->each(function (Model $m) use ($fk) {
-                        $m->$fk = null;
-                        $m->save();
-                    });
-
-                    $this->model->$name()->saveMany($models);
-                    break;
-
-                case 'MorphMany':
-                    // Emulating a sync method by first removing and then adding related models
-                    $fk = $this->model->$name()->getForeignKeyName();
-                    $ft = $this->model->$name()->getMorphType();
-                    $this->model->$name->filter(function (Model $m) use ($ids) {
-                        return !in_array($m->id, $ids);
-                    })->each(function (Model $m) use ($fk, $ft) {
-                        $m->$fk = null;
-                        $m->$ft = null;
-                        $m->save();
-                    });
-
-                    $this->model->$name()->saveMany($models);
-                    break;
-
-                default:
-                    throw new UnhandleableRelationshipException($relation);
             }
         }
 
